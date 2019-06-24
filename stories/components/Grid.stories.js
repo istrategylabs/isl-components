fd1f9b2440975185f001e509fa0a49acc89a5afe
Alex Barbato <alex.j.barbato@gmail.com>
2019-06-24 14:44:37 -0400

    Version bumping storybook and finessing the knob a little for the grid system

--- stories/components/Grid.stories.js
+++ stories/components/Grid.stories.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
+import { number } from '@storybook/addon-knobs'
+
 import { Grid, Column } from '../../src'
 
 const GrayCol = styled(Column)`
@@ -9,17 +11,29 @@ const GrayCol = styled(Column)`
 `
 
 storiesOf('Components', module).add('Grid', () => {
+  const columnWidth = number('Column width', 3, {
+    range: true,
+    min: 1,
+    max: 12,
+    step: 1,
+  })
+  const numColumns = Math.floor(12 / columnWidth)
+  let columns = []
+
+  for (let i = 0; i < numColumns; i++) {
+    columns.push(
+      <GrayCol width={columnWidth} key={i}>
+        Dynamic: {columnWidth}
+      </GrayCol>
+    )
+  }
+
   return (
     <div>
       <Grid>
         <GrayCol width={12}>Column: Width 12</GrayCol>
       </Grid>
-      <Grid>
-        <GrayCol width={3}>Column: Width 3</GrayCol>
-        <GrayCol width={3}>Column: Width 3</GrayCol>
-        <GrayCol width={3}>Column: Width 3</GrayCol>
-        <GrayCol width={3}>Column: Width 3</GrayCol>
-      </Grid>
+      <Grid>{columns}</Grid>
     </div>
   )
 })
